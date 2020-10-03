1a5a3ffe19285711b1673a273b638faaee46b3e6
Charles Kornoelje <charleskornoelje@gmail.com>
2020-10-03 09:00:56 -0400

    update folder structure and auto format

--- client-course-schedulizer/README.md
+++ client-course-schedulizer/README.md
@@ -1,3 +1,5 @@
+# Read Me
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
       </header>
     </div>
   );
-}
-
-export default App;
+};
