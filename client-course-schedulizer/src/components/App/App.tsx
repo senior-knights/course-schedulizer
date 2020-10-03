70a00da6eda9e63dfeb45fd640667daaa050a248
Charles Kornoelje <charleskornoelje@gmail.com>
2020-10-03 17:32:05 -0400

    add scss and bootstrap

--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
-import "./App.css";
+import "./App.scss";
 
 export const App = () => {
   return (
--- client-course-schedulizer/src/index.tsx
+++ client-course-schedulizer/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components";
 import * as serviceWorker from "./serviceWorker";
-import "./styles/index.css";
+import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
