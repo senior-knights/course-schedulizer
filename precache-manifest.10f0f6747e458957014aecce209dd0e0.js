d3cda3d5526b6f2bce5fb3979bfa135894ea8902
Syobnaf <Syobnaf@users.noreply.github.com>
2020-10-08 20:39:24 +0000

    deploy: 37b80866f09439a194bb383170cd499283b91e17

---
+++ precache-manifest.10f0f6747e458957014aecce209dd0e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675f11bc5891c5522cce6c043219ca03",
+    "url": "/course-schedulizer/index.html"
+  },
+  {
+    "revision": "a868088d04246cfc08d1",
+    "url": "/course-schedulizer/static/css/main.19dc362a.chunk.css"
+  },
+  {
+    "revision": "e9efc2147b5763ca33ee",
+    "url": "/course-schedulizer/static/js/2.1d4e67c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course-schedulizer/static/js/2.1d4e67c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a868088d04246cfc08d1",
+    "url": "/course-schedulizer/static/js/main.b8085219.chunk.js"
+  },
+  {
+    "revision": "e5f4fa1a56dc3a3494e8",
+    "url": "/course-schedulizer/static/js/runtime-main.fc402103.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/course-schedulizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
