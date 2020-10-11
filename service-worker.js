540f618cee09888e27a122fa146eb67295e5cbdd
Syobnaf <Syobnaf@users.noreply.github.com>
2020-10-11 19:11:06 +0000

    deploy: e7dee49b5196f1742b95c0843312388dc8cd04b7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/course-schedulizer/precache-manifest.10f0f6747e458957014aecce209dd0e0.js"
+  "./precache-manifest.2eaeeacde532bcfab5f006037e52c2ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/course-schedulizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
