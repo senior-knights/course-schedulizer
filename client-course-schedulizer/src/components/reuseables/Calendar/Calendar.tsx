fcdb7416175d73e4b46c4bfd5e2c9655f1ef8c67
Charles Kornoelje <charleskornoelje@gmail.com>
2020-10-27 21:09:18 -0400

    add a placeholder event

--- client-course-schedulizer/src/components/reuseables/Calendar/Calendar.tsx
+++ client-course-schedulizer/src/components/reuseables/Calendar/Calendar.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import-order-alphabetical/order */
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar, { EventInput } from "@fullcalendar/react";
 
 // Plugins
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -9,13 +9,33 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import React from "react";
 import "./Calendar.scss";
 
+// TODO: remove
+const events: EventInput = [
+  {
+    description: "Lecture",
+    // NOTE: we need to make it be the current data
+    // Or make the calendar start at a certain date
+    end: "2020-10-27T11:30:00",
+    extendedProps: {
+      department: "CS",
+      professor: "VanderLinden",
+    },
+    start: "2020-10-27T10:30:00",
+    title: "CS262",
+  },
+];
+
 export const Calendar = () => {
   return (
     <>
       <FullCalendar
         allDaySlot={false}
         dayHeaderFormat={{ weekday: "long" }}
+        droppable
+        editable
+        events={events}
         headerToolbar={false}
+        height="auto"
         initialView="timeGridWeek"
         nowIndicator={false}
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
