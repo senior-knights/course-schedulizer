a413c2b8e28b7beb91ec567192f94652b6fc15f0
Syobnaf <bryantgeorge1@sbcglobal.net>
2020-10-30 11:31:53 -0400

    improve parsing loops

--- client-course-schedulizer/src/utilities/helpers/readCSV.ts
+++ client-course-schedulizer/src/utilities/helpers/readCSV.ts
@@ -92,61 +92,35 @@ const convertToInterface = function convertToInterface(objects: papa.ParseResult
   const { fields } = meta;
 
   // From the CSV fields, take the ones which we recognize
-  // eslint-disable-next-line prefer-const
-  const usableFields: string[] = [];
-  let field;
-  let startTimeIdx = -1;
-  let startTimeStrIdx = -1;
-  let instructorIdx = -1;
-  let instructorsIdx = -1;
-  let prefixIdx = -1;
-  let prefixesIdx = -1;
+  let usableFields: string[] = [];
+  const duplicates = [
+    ["startTime", "startTimeStr"],
+    ["prefix", "prefixes"],
+    ["instructor", "instructors"],
+  ];
   if (fields) {
-    for (let i = 0; i < fields.length; i += 1) {
-      field = fields[i];
-      if (validFields.includes(field)) {
-        usableFields.push(field);
-        if (field === "startTime") {
-          startTimeIdx = usableFields.length - 1;
-        } else if (field === "startTimeStr") {
-          startTimeStrIdx = usableFields.length - 1;
-        } else if (field === "instructor") {
-          instructorIdx = usableFields.length - 1;
-        } else if (field === "instructors") {
-          instructorsIdx = usableFields.length - 1;
-        } else if (field === "prefix") {
-          prefixIdx = usableFields.length - 1;
-        } else if (field === "prefixes") {
-          prefixesIdx = usableFields.length - 1;
-        }
-      }
-    }
+    usableFields = fields.filter((field) => {
+      return validFields.includes(field);
+    });
   }
-
+  // Remove the duplicate field names
   // If both "startTime" and "startTimeStr" are present, ignore "startTime"
   // This is because Pruim's data has a timestamp in "startTime" (not our timezone)
-  if (startTimeIdx !== -1 && startTimeStrIdx !== -1) {
-    usableFields.splice(startTimeIdx, 1);
-  }
   // If both "instructor" and "instructors" are present, ignore "instructor"
-  if (instructorIdx !== -1 && instructorsIdx !== -1) {
-    usableFields.splice(instructorIdx, 1);
-  }
   // If both "prefix" and "prefixes" are present, ignore "prefix"
-  if (prefixIdx !== -1 && prefixesIdx !== -1) {
-    usableFields.splice(prefixIdx, 1);
-  }
+  duplicates.forEach((duplicate) => {
+    if (usableFields.includes(duplicate[0]) && usableFields.includes(duplicate[1])) {
+      usableFields.splice(usableFields.indexOf(duplicate[0]), 1);
+    }
+  });
 
   // Parse each row of the CSV as an object
-  let object;
   let value: string;
   let regMatch: RegExpMatchArray | null;
   let roomParts: string[];
   let names: string[];
   let nameParts: string[];
-  for (let i = 0; i < data.length; i += 1) {
-    object = data[i];
-
+  data.forEach((object) => {
     // Reset defaults
     sss = {
       ampm: "AM",
@@ -176,211 +150,208 @@ const convertToInterface = function convertToInterface(objects: papa.ParseResult
     };
 
     // Iterate through the fields of the CSV, and parse their values for this object
-    for (let j = 0; j < usableFields.length; j += 1) {
-      field = usableFields[j];
-      if (field in object) {
-        value = String(object[field]);
-        switch (field) {
-          case "name": {
-            sss.name = value;
-            break;
-          }
-          case "prefixes":
-          case "prefix": {
-            sss.prefixes = value.replace(" ", "").split(/[;,]/);
-            break;
-          }
-          case "number": {
-            sss.number = value;
-            break;
-          }
-          case "section": {
-            sss.letter = value;
-            break;
-          }
-          case "studentHours": {
-            sss.studentHours = Number(value);
-            break;
-          }
-          case "facultyHours": {
-            sss.facultyHours = Number(value);
-            break;
-          }
-          case "startTimeStr":
-          case "startTime": {
-            // Look for a time
-            regMatch = value.match(timeReg);
-            if (regMatch != null && regMatch.length === 3) {
-              // Get the hour and minute values, store as number and strings
-              [, sss.hourPart, sss.minPart] = regMatch;
-              sss.numHourPart = Number(sss.hourPart);
+    usableFields.forEach((field) => {
+      value = String(object[field]);
+      switch (field) {
+        case "name": {
+          sss.name = value;
+          break;
+        }
+        case "prefixes":
+        case "prefix": {
+          sss.prefixes = value.replace(" ", "").split(/[;,]/);
+          break;
+        }
+        case "number": {
+          sss.number = value;
+          break;
+        }
+        case "section": {
+          sss.letter = value;
+          break;
+        }
+        case "studentHours": {
+          sss.studentHours = Number(value);
+          break;
+        }
+        case "facultyHours": {
+          sss.facultyHours = Number(value);
+          break;
+        }
+        case "startTimeStr":
+        case "startTime": {
+          // Look for a time
+          regMatch = value.match(timeReg);
+          if (regMatch != null && regMatch.length === 3) {
+            // Get the hour and minute values, store as number and strings
+            [, sss.hourPart, sss.minPart] = regMatch;
+            sss.numHourPart = Number(sss.hourPart);
 
-              // Handle high hour values
-              if (sss.numHourPart > 11) {
-                if (sss.numHourPart > 12) {
-                  // If military time, convert to standard
-                  sss.numHourPart -= 12;
-                  sss.hourPart = String(sss.numHourPart);
-                }
-                // Assume PM when 12:XX or military time
-                sss.ampm = "PM";
+            // Handle high hour values
+            if (sss.numHourPart > 11) {
+              if (sss.numHourPart > 12) {
+                // If military time, convert to standard
+                sss.numHourPart -= 12;
+                sss.hourPart = String(sss.numHourPart);
               }
+              // Assume PM when 12:XX or military time
+              sss.ampm = "PM";
+            }
 
-              // If hour is 0, assume military time of 12 AM
-              if (sss.numHourPart === 0) {
-                sss.hourPart = "12";
-                sss.numHourPart = 12;
-              }
+            // If hour is 0, assume military time of 12 AM
+            if (sss.numHourPart === 0) {
+              sss.hourPart = "12";
+              sss.numHourPart = 12;
+            }
 
-              // Look to see whether AM or PM is specified explicitly
-              if (pmReg.test(value)) {
-                sss.ampm = "PM";
-                if (amReg.test(value)) {
-                  // eslint-disable-next-line no-console
-                  console.log(`Time of "${value}" is labeled with AM and PM, defaulting to PM`);
-                }
-              } else if (amReg.test(value)) {
-                sss.ampm = "AM";
+            // Look to see whether AM or PM is specified explicitly
+            if (pmReg.test(value)) {
+              sss.ampm = "PM";
+              if (amReg.test(value)) {
+                // eslint-disable-next-line no-console
+                console.log(`Time of "${value}" is labeled with AM and PM, defaulting to PM`);
               }
+            } else if (amReg.test(value)) {
+              sss.ampm = "AM";
+            }
 
-              // Piece the time together
-              sss.startTime = `${sss.hourPart}:${sss.minPart} ${sss.ampm}`;
+            // Piece the time together
+            sss.startTime = `${sss.hourPart}:${sss.minPart} ${sss.ampm}`;
+          } else {
+            // eslint-disable-next-line no-console
+            console.log(`Time of "${value}" is unreadable, defaulting to 8:00 AM`);
+          }
+          break;
+        }
+        case "duration": {
+          sss.duration = Number(value);
+          break;
+        }
+        case "location": {
+          roomParts = value.trim().split(" ");
+          if (roomParts.length === 1) {
+            // No room number given
+            [sss.building] = roomParts;
+            sss.roomNumber = "";
+          } else if (roomParts.length === 2) {
+            // Building and room number given
+            [sss.building, sss.roomNumber] = roomParts;
+          } else {
+            // Too many room parts given, assume last part is room number and rest is building
+            sss.building = roomParts.slice(0, -1).join(" ");
+            [sss.roomNumber] = roomParts.slice(-1);
+          }
+          break;
+        }
+        case "roomCapacity": {
+          sss.roomCapacity = Number(value);
+          break;
+        }
+        case "year": {
+          sss.year = Number(value);
+          break;
+        }
+        case "term": {
+          if (fallReg.test(value)) {
+            sss.term = di.Term.Fall;
+          } else if (summerReg.test(value)) {
+            sss.term = di.Term.Summer;
+          } else if (springReg.test(value)) {
+            sss.term = di.Term.Spring;
+          } else if (interimReg.test(value)) {
+            sss.term = di.Term.Interim;
+          } else {
+            // eslint-disable-next-line no-console
+            console.log(`Term of "${value}" is unreadable, defaulting to Fall`);
+          }
+          break;
+        }
+        case "half": {
+          if (firstReg.test(value)) {
+            sss.half = di.Half.First;
+          } else if (secondReg.test(value)) {
+            sss.half = di.Half.Second;
+          } else if (fullReg.test(value)) {
+            sss.half = di.Half.Full;
+          } else {
+            // eslint-disable-next-line no-console
+            console.log(`Half of "${value}" is unreadable, defaulting to Full`);
+          }
+          break;
+        }
+        case "days": {
+          sss.days = [];
+          if (sunReg.test(value)) {
+            sss.days.push(di.Day.Sunday);
+          }
+          if (monReg.test(value)) {
+            sss.days.push(di.Day.Monday);
+          }
+          if (tuesReg.test(value)) {
+            sss.days.push(di.Day.Tuesday);
+          }
+          if (wedReg.test(value)) {
+            sss.days.push(di.Day.Wednesday);
+          }
+          if (thursReg.test(value)) {
+            sss.days.push(di.Day.Thursday);
+          }
+          if (friReg.test(value)) {
+            sss.days.push(di.Day.Friday);
+          }
+          if (satReg.test(value)) {
+            sss.days.push(di.Day.Saturday);
+          }
+          break;
+        }
+        case "globalMax": {
+          sss.globalMax = Number(value);
+          break;
+        }
+        case "localMax": {
+          sss.localMax = Number(value);
+          break;
+        }
+        case "anticipatedSize": {
+          sss.anticipatedSize = Number(value);
+          break;
+        }
+        case "instructors":
+        case "instructor": {
+          names = value.split(/[;,]/);
+          names.forEach((name) => {
+            nameParts = name.trim().split(" ");
+            if (nameParts.length === 1) {
+              // No last name given
+              sss.instructors.push({
+                firstName: nameParts[0],
+                lastName: "",
+              });
+            } else if (nameParts.length === 2) {
+              // First and last given
+              sss.instructors.push({
+                firstName: nameParts[0],
+                lastName: nameParts[1],
+              });
             } else {
-              // eslint-disable-next-line no-console
-              console.log(`Time of "${value}" is unreadable, defaulting to 8:00 AM`);
+              // Too many names given, assume first part is first name and rest is last name
+              sss.instructors.push({
+                firstName: nameParts[0],
+                lastName: nameParts.slice(1).join(" "),
+              });
             }
-            break;
-          }
-          case "duration": {
-            sss.duration = Number(value);
-            break;
-          }
-          case "location": {
-            roomParts = value.trim().split(" ");
-            if (roomParts.length === 1) {
-              // No room number given
-              [sss.building] = roomParts;
-              sss.roomNumber = "";
-            } else if (roomParts.length === 2) {
-              // Building and room number given
-              [sss.building, sss.roomNumber] = roomParts;
-            } else {
-              // Too many room parts given, assume last part is room number and rest is building
-              sss.building = roomParts.slice(0, -1).join(" ");
-              [sss.roomNumber] = roomParts.slice(-1);
-            }
-            break;
-          }
-          case "roomCapacity": {
-            sss.roomCapacity = Number(value);
-            break;
-          }
-          case "year": {
-            sss.year = Number(value);
-            break;
-          }
-          case "term": {
-            if (fallReg.test(value)) {
-              sss.term = di.Term.Fall;
-            } else if (summerReg.test(value)) {
-              sss.term = di.Term.Summer;
-            } else if (springReg.test(value)) {
-              sss.term = di.Term.Spring;
-            } else if (interimReg.test(value)) {
-              sss.term = di.Term.Interim;
-            } else {
-              // eslint-disable-next-line no-console
-              console.log(`Term of "${value}" is unreadable, defaulting to Fall`);
-            }
-            break;
-          }
-          case "half": {
-            if (firstReg.test(value)) {
-              sss.half = di.Half.First;
-            } else if (secondReg.test(value)) {
-              sss.half = di.Half.Second;
-            } else if (fullReg.test(value)) {
-              sss.half = di.Half.Full;
-            } else {
-              // eslint-disable-next-line no-console
-              console.log(`Half of "${value}" is unreadable, defaulting to Full`);
-            }
-            break;
-          }
-          case "days": {
-            sss.days = [];
-            if (sunReg.test(value)) {
-              sss.days.push(di.Day.Sunday);
-            }
-            if (monReg.test(value)) {
-              sss.days.push(di.Day.Monday);
-            }
-            if (tuesReg.test(value)) {
-              sss.days.push(di.Day.Tuesday);
-            }
-            if (wedReg.test(value)) {
-              sss.days.push(di.Day.Wednesday);
-            }
-            if (thursReg.test(value)) {
-              sss.days.push(di.Day.Thursday);
-            }
-            if (friReg.test(value)) {
-              sss.days.push(di.Day.Friday);
-            }
-            if (satReg.test(value)) {
-              sss.days.push(di.Day.Saturday);
-            }
-            break;
-          }
-          case "globalMax": {
-            sss.globalMax = Number(value);
-            break;
-          }
-          case "localMax": {
-            sss.localMax = Number(value);
-            break;
-          }
-          case "anticipatedSize": {
-            sss.anticipatedSize = Number(value);
-            break;
-          }
-          case "instructors":
-          case "instructor": {
-            names = value.split(/[;,]/);
-            for (let n = 0; n < names.length; n += 1) {
-              nameParts = names[n].trim().split(" ");
-              if (nameParts.length === 1) {
-                // No last name given
-                sss.instructors.push({
-                  firstName: nameParts[0],
-                  lastName: "",
-                });
-              } else if (nameParts.length === 2) {
-                // First and last given
-                sss.instructors.push({
-                  firstName: nameParts[0],
-                  lastName: nameParts[1],
-                });
-              } else {
-                // Too many names given, assume first part is first name and rest is last name
-                sss.instructors.push({
-                  firstName: nameParts[0],
-                  lastName: nameParts.slice(1).join(" "),
-                });
-              }
-            }
-            break;
-          }
-          case "comments": {
-            sss.comments = value;
-            break;
-          }
-          default: {
-            break;
-          }
+          });
+          break;
+        }
+        case "comments": {
+          sss.comments = value;
+          break;
+        }
+        default: {
+          break;
         }
       }
-    }
+    });
 
     // Create a section for this row of the CSV, and add it to the schedule
     const section: di.Section = {
@@ -416,7 +387,7 @@ const convertToInterface = function convertToInterface(objects: papa.ParseResult
       year: sss.year,
     };
     schedule.sections.push(section);
-  }
+  });
   return schedule;
 };
 
@@ -428,6 +399,6 @@ export const readCSV = function readCSV() {
     })
     .then((text) => {
       // eslint-disable-next-line no-console
-      console.log(convertToInterface(papa.parse(text, { header: true })));
+      console.log(convertToInterface(papa.parse(text, { header: true, skipEmptyLines: true })));
     });
 };
