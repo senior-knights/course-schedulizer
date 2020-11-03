5edbee49f557cc3b562a79bbebb7aced37af3708
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-03 08:58:56 -0500

    cleanup and add todos

--- client-course-schedulizer/src/components/Tabs/FacultySchedule/FacultySchedule.tsx
+++ client-course-schedulizer/src/components/Tabs/FacultySchedule/FacultySchedule.tsx
@@ -16,32 +16,23 @@ const professors = [
   "Plantinga",
 ];
 
+/* Creates a list of Calendars to create the Faculty Schedule
+  <Stick> is used to stick the Schedule Header to the Schedule
+  to track horizontal scrolling.
+*/
 export const FacultySchedule = () => {
   return (
     <>
       <ScheduleToolbar />
-      <div className="calendars-wrapper">
-        <Stick
-          autoFlipVertically
-          node={
-            <StickyNode top={window.innerHeight * 0.1}>
-              <div className="calendars-adjacent calendar-title-row">
-                {professors.map((prof) => {
-                  return (
-                    <div key={prof} className="calendar calendar-title">
-                      {prof}
-                    </div>
-                  );
-                })}
-              </div>
-            </StickyNode>
-          }
-          position="top left"
-        >
-          <div className="calendars-adjacent">
+      <div className="schedule-wrapper">
+        <Stick node={<StickyHeader />} position="top left">
+          <div className="adjacent">
             {professors.map((prof, index) => {
+              const hideAxis = index !== 0 ? "hide-axis" : "";
+
               return (
-                <div key={prof} className={`calendar ${index !== 0 ? "hide-axis" : ""}`}>
+                // {/* TODO: Fix this because the first calendar is smaller */}
+                <div key={prof} className={`calendar-width ${hideAxis}`}>
                   <Calendar key={prof} />
                 </div>
               );
@@ -50,5 +41,26 @@ export const FacultySchedule = () => {
         </Stick>
       </div>
     </>
+  );
+};
+
+/*
+  StickyHeader is used to keep the Schedule header sticky to the
+  top of the view port.
+*/
+// TODO: Make into reusable component for future room schedule
+const StickyHeader = () => {
+  return (
+    <StickyNode top={window.innerHeight / 10}>
+      <div className="adjacent schedule-header-row">
+        {professors.map((prof) => {
+          return (
+            <div key={prof} className="calendar-width calendar-title">
+              {prof}
+            </div>
+          );
+        })}
+      </div>
+    </StickyNode>
   );
 };
