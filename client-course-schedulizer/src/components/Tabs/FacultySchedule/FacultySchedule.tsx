0ed5e51bea09b5c40de9007c20f1a0476bf6af99
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-02 21:55:01 -0500

    🥳 Make the professors sticky and move!

--- client-course-schedulizer/src/components/Tabs/FacultySchedule/FacultySchedule.tsx
+++ client-course-schedulizer/src/components/Tabs/FacultySchedule/FacultySchedule.tsx
@@ -1,26 +1,53 @@
 import React from "react";
+import StickyNode from "react-stickynode";
+import Stick from "react-stick";
 import { Calendar } from "../../reuseables/Calendar";
 import { ScheduleToolbar } from "../../Toolbar/ScheduleToolbar";
 import "./FacultySchedule.scss";
 
 // TODO: remove this
-const professors = ["Norman", "Adams", "VanderLinden", "Arnold", "Bailey"];
+const professors = [
+  "Norman",
+  "Adams",
+  "VanderLinden",
+  "Arnold",
+  "Bailey",
+  "Schuurman",
+  "Plantinga",
+];
 
 export const FacultySchedule = () => {
   return (
     <>
       <ScheduleToolbar />
       <div className="calendars-wrapper">
-        <div className="calendars-adjacent">
-          {professors.map((prof, index) => {
-            return (
-              <div key={prof} className={`calendar ${index !== 0 ? "hide-axis" : ""}`}>
-                <h3>{prof}</h3>
-                <Calendar key={prof} />
+        <Stick
+          autoFlipVertically
+          node={
+            <StickyNode>
+              <div className="calendars-adjacent">
+                {professors.map((prof) => {
+                  return (
+                    <div key={prof} className="calendar calendar-title">
+                      {prof}
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
+            </StickyNode>
+          }
+          position="top left"
+        >
+          <div className="calendars-adjacent">
+            {professors.map((prof, index) => {
+              return (
+                <div key={prof} className={`calendar ${index !== 0 ? "hide-axis" : ""}`}>
+                  <Calendar key={prof} />
+                </div>
+              );
+            })}
+          </div>
+        </Stick>
       </div>
     </>
   );
