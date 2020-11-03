c754e08bfb862bedc4768f25117606861c8effe9
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-02 21:51:43 -0500

    add references to fake types

--- client-course-schedulizer/src/types/react-stick.d.ts
+++ client-course-schedulizer/src/types/react-stick.d.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
-// declaring module will allow typescript to import the module
+// TODO: add types
+// ref: https://stackoverflow.com/questions/48224858/import-require-nodejs-modules-without-types-in-typescript-in-2018
 declare module "react-stick" {
   const Stick: any;
   export default Stick;
