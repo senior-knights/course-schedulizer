62f0ad298c4a19ce2230d3f7583efa77bf64cb0f
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-05 13:40:19 -0500

    add ScheduleContext

--- client-course-schedulizer/src/components/Header/Header/Header.tsx
+++ client-course-schedulizer/src/components/Header/Header/Header.tsx
@@ -1,13 +1,16 @@
 import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from "@material-ui/core";
 import { Menu as MenuIcon } from "@material-ui/icons";
 import { bindMenu, bindTrigger, usePopupState } from "material-ui-popup-state/hooks";
-import React from "react";
+import React, { useContext } from "react";
 import logo from "../../../assets/CalvinUniv-vert-full-color-inverse.png";
 import { ImportButton } from "../ImportButton";
+import * as writeCSV from "../../../utilities/helpers/writeCSV";
+import { ScheduleContext } from "../../../utilities/services/context";
 import "./Header.scss";
 
 export const Header = () => {
   const popupState = usePopupState({ popupId: "menu", variant: "popover" });
+  const { schedule } = useContext(ScheduleContext);
 
   return (
     <div className="header">
@@ -20,7 +23,16 @@ export const Header = () => {
             <MenuItem>
               <ImportButton />
             </MenuItem>
-            <MenuItem onClick={popupState.close}>Export CSV</MenuItem>
+            <MenuItem
+              onClick={() => {
+                // TODO: make an ExportButton component
+                popupState.close();
+                // eslint-disable-next-line no-console
+                console.log(writeCSV.scheduleToCSVString(schedule));
+              }}
+            >
+              Export CSV
+            </MenuItem>
           </Menu>
           <Typography variant="h6">Course Schedulizer</Typography>
           <img alt="Org Logo" className="org-logo" src={logo} />
