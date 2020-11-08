b9b8de44887b70171b93c7c889843c1ff8235306
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-07 19:24:04 -0500

    Conditionally display tabs

--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -4,11 +4,11 @@ import { Tabs } from "../Tabs";
 import "./App.scss";
 import { ScheduleContext } from "../../utilities/services/context";
 import { Schedule } from "../../utilities/interfaces/dataInterfaces";
-import { fakeSchedule } from "../../utilities/services/fakeSchedule";
+// import { fakeSchedule } from "../../utilities/services/fakeSchedule";
 
 export const App = () => {
-  // const [schedule, setSchedule] = useState<Schedule>({ courses: [] });
-  const [schedule, setSchedule] = useState<Schedule>(fakeSchedule as Schedule);
+  const [schedule, setSchedule] = useState<Schedule>({ courses: [] });
+  // const [schedule, setSchedule] = useState<Schedule>(fakeSchedule as Schedule);
 
   return (
     <div className="App">
--- client-course-schedulizer/src/components/Tabs/Tabs.tsx
+++ client-course-schedulizer/src/components/Tabs/Tabs.tsx
@@ -1,8 +1,9 @@
 import { Box, Tabs as MUITabs, Paper, Tab, Typography } from "@material-ui/core";
-import React, { ChangeEvent, PropsWithChildren, useState } from "react";
+import React, { ChangeEvent, PropsWithChildren, useContext, useState } from "react";
 import { FacultySchedule } from "./FacultySchedule";
 import { ScheduleToolbar } from "../Toolbar/ScheduleToolbar";
 import "./Tabs.scss";
+import { ScheduleContext } from "../../utilities/services/context";
 
 interface TabPanelProps {
   index: number;
@@ -31,6 +32,7 @@ const TabPanel = (props: PropsWithChildren<TabPanelProps>) => {
 
 export const Tabs = () => {
   const [tabValue, setTabValue] = useState(0);
+  const { schedule } = useContext(ScheduleContext);
 
   const handleTabChange = (event: ChangeEvent<{}>, newValue: number) => {
     setTabValue(newValue);
@@ -38,30 +40,36 @@ export const Tabs = () => {
 
   return (
     <Paper>
-      <MUITabs
-        centered
-        indicatorColor="primary"
-        onChange={handleTabChange}
-        textColor="primary"
-        value={tabValue}
-      >
-        <Tab label="Faculty Schedule" />
-        <Tab label="Room Schedule" />
-        <Tab label="Teaching Loads" />
-        <Tab label="Conflicts" />
-      </MUITabs>
-      <TabPanel index={0} value={tabValue}>
-        <FacultySchedule />
-      </TabPanel>
-      <TabPanel index={1} value={tabValue}>
-        <ScheduleToolbar />
-      </TabPanel>
-      <TabPanel index={2} value={tabValue}>
-        Item Three
-      </TabPanel>
-      <TabPanel index={3} value={tabValue}>
-        Item Four
-      </TabPanel>
+      {schedule.courses.length === 0 ? (
+        <h2>No schedule selected. Please import a CSV to start Editing</h2>
+      ) : (
+        <>
+          <MUITabs
+            centered
+            indicatorColor="primary"
+            onChange={handleTabChange}
+            textColor="primary"
+            value={tabValue}
+          >
+            <Tab label="Faculty Schedule" />
+            <Tab label="Room Schedule" />
+            <Tab label="Teaching Loads" />
+            <Tab label="Conflicts" />
+          </MUITabs>
+          <TabPanel index={0} value={tabValue}>
+            <FacultySchedule />
+          </TabPanel>
+          <TabPanel index={1} value={tabValue}>
+            <ScheduleToolbar />
+          </TabPanel>
+          <TabPanel index={2} value={tabValue}>
+            Item Three
+          </TabPanel>
+          <TabPanel index={3} value={tabValue}>
+            Item Four
+          </TabPanel>
+        </>
+      )}
     </Paper>
   );
 };
