1c9865866e1317e49b0d5e56cc9272147e406e73
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-18 19:50:59 -0500

    make creating child components reusable

--- client-course-schedulizer/src/components/reuseables/AsyncComponent/AsyncComponent.tsx
+++ client-course-schedulizer/src/components/reuseables/AsyncComponent/AsyncComponent.tsx
@@ -1,20 +1,31 @@
-import React, { PropsWithChildren, FC, ReactNode } from "react";
+import React, { PropsWithChildren, FC } from "react";
+import { Literal, Static, Union } from "runtypes";
+import { createFC, getElements } from "../../../utilities/helpers/makeFC";
 
 interface AsyncComponent {
   isLoading: boolean;
 }
 
-type SubComponents = "Loading" | "Loaded";
-type AsyncSubComponents = { [key in SubComponents]: FC<{}> };
+// Define the runtype
+const SubComponents = Union(Literal("Loading"), Literal("Loaded"));
 
-const getSubComponent = (children: ReactNode, displayName: string) => {
-  return React.Children.map(children as JSX.Element[], (child: JSX.Element) => {
-    return child?.type.displayName === displayName ? child : null;
-  });
-};
+// Some boiler plate to get useable types.
+type SubComponents = Static<typeof SubComponents>;
+type AsyncSubComponents = { [key in SubComponents]: FC<{}> };
 
 /*
   Show a loading state while waiting on a async function to finish.
+
+  Usage:
+  <AsyncComponent isLoading={boolean}>
+    <AsyncComponent.Loading>
+      content to display while loading
+    </AsyncComponent.Loading>
+    <AsyncComponent.Loaded>
+      content to display when loading is complete
+    </AsyncComponent.Loaded>
+  </AsyncComponent>
+
   References:
     - https://stackoverflow.com/a/56953600/9931154
     - https://dev.to/shayanypn/buckle-with-react-sub-component-10ll
@@ -23,49 +34,11 @@ export const AsyncComponent: FC<AsyncComponent> & AsyncSubComponents = ({
   children,
   isLoading,
 }: PropsWithChildren<AsyncComponent>) => {
-  // TODO: better way to get all of the children? Loop?
-  const loadedNode = getSubComponent(children, "Loaded");
-  const loadingNode = getSubComponent(children, "Loading");
+  const { Loading, Loaded } = getElements(children, SubComponents);
 
-  return <>{isLoading ? <>{loadingNode}</> : <>{loadedNode}</>}</>;
+  return <>{isLoading ? <>{Loading}</> : <>{Loaded}</>}</>;
 };
 
-const Loading: FC<{}> = ({ children }: PropsWithChildren<{}>): JSX.Element => {
-  return <>{children}</>;
-};
-Loading.displayName = "Loading";
-AsyncComponent.Loading = Loading;
-
-const Loaded: FC<{}> = ({ children }: PropsWithChildren<{}>): JSX.Element => {
-  return <>{children}</>;
-};
-Loaded.displayName = "Loaded";
-AsyncComponent.Loaded = Loaded;
-
-/// TODO: some work trying to make this easier to update. I think I'm close but couldn't get it to work.
-// https://gitlab.com/cdaringe/react-auto-subcomponent
-
-// AsyncComponent.displayName = "AsyncComponent";
-// AsyncComponent.defaultProps = {
-//   displayName: "AsyncComponent",
-//   isLoading: false,
-// };
-
-// const makeSubComp = <T extends object, K extends keyof T & string>(
-//   Component: AsyncComponent,
-//   subCompName: K,
-// ) => {
-//   const SubComp: FC<{}> = ({ children }: PropsWithChildren<{}>) => {
-//     return <>{children}</>;
-//   };
-//   return new Proxy(Component, {
-//     get: function handleGetAutoComponent(obj, prop) {
-//       const ChildComponent = SubComp;
-//       ChildComponent.displayName = `${Component.displayName}.${ChildComponent.displayName}`;
-//       return ChildComponent;
-//     },
-//   });
-// };
-
-// AsyncComponent = makeSubComp(AsyncComponent, "Loading");
-// AsyncComponent = makeSubComp(AsyncComponent, "Loaded");
+// Define sub-components.
+AsyncComponent.Loading = createFC("Loading");
+AsyncComponent.Loaded = createFC("Loaded");
--- client-course-schedulizer/src/utilities/helpers/utils.ts
+++ client-course-schedulizer/src/utilities/helpers/utils.ts
@@ -1,3 +1,5 @@
+import { Children, ReactNode } from "react";
+
 /* Create an iterable from an enumeration */
 export const enumArray = <T>(e: T) => {
   return Object.keys(e).map((val) => {
@@ -8,3 +10,12 @@ export const enumArray = <T>(e: T) => {
 /* a no-op fn used for default values */
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const voidFn = () => {};
+
+/* Method of getting a child component by their display name.
+  Used in sub-components.
+*/
+export const getChildByName = (children: ReactNode, displayName: string) => {
+  return Children.map(children as JSX.Element[], (child: JSX.Element) => {
+    return child?.type.displayName === displayName ? child : null;
+  });
+};
