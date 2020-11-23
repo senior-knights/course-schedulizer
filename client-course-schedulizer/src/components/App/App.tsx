8c0ad8fc75635851250f173c4dafe52ec482c101
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-23 14:50:45 -0500

    add routing to home, about, help

--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import React, { useReducer, useState } from "react";
-import { Header } from "../Header/Header";
-import { Tabs } from "../Tabs";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
 import { AppContext } from "../../utilities/services/appContext";
 import { reducer } from "../../utilities/services/appReducer";
 import { initialAppState } from "../../utilities/interfaces/appInterfaces";
+import { AboutPage, HelpPage, SchedulizerPage } from "../Pages";
 
 export const App = () => {
   const [isCSVLoading, setIsCSVLoading] = useState(false);
@@ -12,10 +12,34 @@ export const App = () => {
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ appDispatch, appState, isCSVLoading, setIsCSVLoading }}>
-        <Header />
-        <Tabs />
-      </AppContext.Provider>
+      <Router>
+        <AppContext.Provider value={{ appDispatch, appState, isCSVLoading, setIsCSVLoading }}>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/help">
+              <HelpPage />
+            </Route>
+            <Route path="/">
+              <SchedulizerPage />
+            </Route>
+          </Switch>
+        </AppContext.Provider>
+        <nav style={{ bottom: 0, position: "sticky" }}>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          <div>
+            <Link to="/help">Help</Link>
+          </div>
+        </nav>
+      </Router>
     </div>
   );
 };
---
+++ client-course-schedulizer/src/components/Pages/SchedulizerPage/SchedulizerPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../../Header/Header";
+import { Tabs } from "../../Tabs";
+
+export const SchedulizerPage = () => {
+  return (
+    <>
+      <Header />
+      <Tabs />
+    </>
+  );
+};
