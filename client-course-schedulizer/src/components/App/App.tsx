f88cb077df7195b0697117f5572e93e2ef4a98cc
Charles Kornoelje <charleskornoelje@gmail.com>
2020-11-24 10:50:01 -0500

    Move context so the CSV stays loaded

--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import { AboutPage, HelpPage, SchedulizerPage } from "../pages";
 import { Header } from "../Header/Header";
+import { reducer } from "../../utilities/services/appReducer";
+import { initialAppState } from "../../utilities/interfaces/appInterfaces";
+import { AppContext } from "../../utilities/services/appContext";
 
 export const App = () => {
+  const [isCSVLoading, setIsCSVLoading] = useState(false);
+  const [appState, appDispatch] = useReducer(reducer, initialAppState);
+
   return (
     <div className="App">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/about">
-            <AboutPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/">
-            <SchedulizerPage />
-          </Route>
-        </Switch>
+        <AppContext.Provider value={{ appDispatch, appState, isCSVLoading, setIsCSVLoading }}>
+          <Switch>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/help">
+              <HelpPage />
+            </Route>
+            <Route path="/">
+              <SchedulizerPage />
+            </Route>
+          </Switch>
+        </AppContext.Provider>
       </Router>
     </div>
   );
--- client-course-schedulizer/src/components/Pages/SchedulizerPage/SchedulizerPage.tsx
+++ client-course-schedulizer/src/components/Pages/SchedulizerPage/SchedulizerPage.tsx
@@ -1,16 +1,10 @@
-import React, { useReducer, useState } from "react";
-import { initialAppState } from "../../../utilities/interfaces/appInterfaces";
-import { AppContext } from "../../../utilities/services/appContext";
-import { reducer } from "../../../utilities/services/appReducer";
+import React from "react";
 import { Tabs } from "../../Tabs";
 
 export const SchedulizerPage = () => {
-  const [isCSVLoading, setIsCSVLoading] = useState(false);
-  const [appState, appDispatch] = useReducer(reducer, initialAppState);
-
   return (
-    <AppContext.Provider value={{ appDispatch, appState, isCSVLoading, setIsCSVLoading }}>
+    <>
       <Tabs />
-    </AppContext.Provider>
+    </>
   );
 };
