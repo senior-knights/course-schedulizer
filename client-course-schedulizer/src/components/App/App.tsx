e45ca02121f43ed712f39de875be39a48e2f92b6
Syobnaf <bryantgeorge1@sbcglobal.net>
2021-02-28 16:11:35 -0500

    read remote csv

--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -1,24 +1,11 @@
 import { Footer, Header } from "components";
 import { AboutPage, HelpPage, SchedulizerPage } from "components/pages";
 import fetch from "isomorphic-fetch";
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import { initialAppState, reducer, useLocal } from "utilities";
+import { csvStringToSchedule, initialAppState, reducer, useLocal } from "utilities";
 import { AppContext } from "utilities/contexts";
 import "./App.scss";
-
-const readRemoteCSV = async () => {
-  // eslint-disable-next-line no-restricted-globals
-  const url = location.href;
-  // We'll need to be more careful about this if we end up using more GET params
-  const csvIndex = url.indexOf("?csv=") + 5;
-  const csvUrl = url.slice(csvIndex);
-  if (csvUrl !== "") {
-    const csv = await fetch(csvUrl);
-    const csvString: string = await csv.text();
-    console.log(csvString);
-  }
-};
 
 /* App with a HashRouter.
 Ref: https://medium.com/@bennirus/deploying-a-create-react-app-with-routing-to-github-pages-f386b6ce84c2
@@ -27,7 +14,27 @@ export const App = () => {
   const [isCSVLoading, setIsCSVLoading] = useState(false);
   const { save } = useLocal("appState");
   const [appState, appDispatch] = useReducer(reducer(save), initialAppState);
-  readRemoteCSV();
+
+  // Ref: https://stackoverflow.com/questions/56663785/invalid-hook-call-hooks-can-only-be-called-inside-of-the-body-of-a-function-com
+  useEffect(() => {
+    // eslint-disable-next-line no-restricted-globals
+    const url = location.href;
+    // We'll need to be more careful about this if we end up using more GET params
+    const csvIndex = url.indexOf("?csv=");
+    if (csvIndex !== -1) {
+      const csvUrl = url.slice(csvIndex + 5);
+      if (csvUrl !== "") {
+        fetch(csvUrl)
+          .then((response) => {
+            return response.text();
+          })
+          .then(async (result) => {
+            const schedule = csvStringToSchedule(result);
+            await appDispatch({ payload: { schedule }, type: "setScheduleData" });
+          });
+      }
+    }
+  }, []);
 
   return (
     <div className="App">
