2941b5c7438dd2b85797ac7b993c0255fa385fb4
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-03-04 20:33:44 -0500

    add FieldArrayForm components

--- client-course-schedulizer/src/components/pages/HarmonyPage/HarmonyPage.tsx
+++ client-course-schedulizer/src/components/pages/HarmonyPage/HarmonyPage.tsx
@@ -1,10 +1,26 @@
+import { FieldArrayForm } from "components";
 import React from "react";
-import { Harmony } from "./Harmony";
+// import { Harmony } from "./Harmony";
 
+/**
+ * The page contains all of the Harmoniously user-experience.
+ *   Allows the user to load professor, courses, rooms, and times from
+ *   the uploaded schedule as well as edit them and create assignments (constraints).
+ * Harmony will find schedules with no conflicts and allow the user to send it back
+ *   to the Schedulizer to make edits.
+ */
 export const HarmonyPage = () => {
   return (
     <>
-      <Harmony />
+      <h2>
+        Note: This page is in a <i>pre-alpha</i> state. Output might not be perfect.
+      </h2>
+      <FieldArrayForm defaultValue={{ First: "", Last: "" }} fieldsName="professors" />
+      <FieldArrayForm defaultValue={{ Course: "" }} fieldsName="courses" />
+      <FieldArrayForm defaultValue={{ Room: "" }} fieldsName="rooms" />
+      <FieldArrayForm defaultValue={{ Time: "" }} fieldsName="times" />
+      {/* TODO: remove this for now */}
+      {/* <Harmony /> */}
     </>
   );
 };
---
+++ client-course-schedulizer/src/components/reuseables/FieldArrayForm/FieldArrayForm.tsx
@@ -0,0 +1,49 @@
+import startCase from "lodash/startCase";
+import toLower from "lodash/toLower";
+import React from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { FieldArrayFormProvider } from "utilities";
+import { FieldArrayFields } from "./FieldArrayFields";
+import { FieldArrayFormActionButtons } from "./FieldArrayFormActionButtons";
+
+interface FieldArrayFormProps {
+  defaultValue: object;
+  fieldsName: string;
+}
+
+/**
+ * FieldArrayForm creates a form with a variable amount of fields all for similar values.
+ * This uses a provider to pass form related data down the component tree.
+ * Handles the form submission and formats the name to look nice on the web.
+ */
+export const FieldArrayForm = ({ fieldsName, defaultValue }: FieldArrayFormProps) => {
+  const titleCaseName = startCase(toLower(fieldsName));
+  const formMethods = useForm({
+    defaultValues: { [titleCaseName]: [defaultValue] },
+  });
+  const { control, handleSubmit } = formMethods;
+  const fieldArrayMethods = useFieldArray({
+    control,
+    name: titleCaseName,
+  });
+
+  return (
+    <FieldArrayFormProvider
+      {...formMethods}
+      {...fieldArrayMethods}
+      defaultValue={defaultValue}
+      titleCaseName={titleCaseName}
+    >
+      <form
+        onSubmit={handleSubmit((data) => {
+          // TODO: handle this.
+          console.log(data);
+        })}
+      >
+        <h3>{titleCaseName}: </h3>
+        <FieldArrayFields />
+        <FieldArrayFormActionButtons />
+      </form>
+    </FieldArrayFormProvider>
+  );
+};
