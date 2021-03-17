ae099b80c15ce0f22330d4003f939eeec749183d
Jonri2 <Jonri2@users.noreply.github.com>
2021-03-17 01:27:29 +0000

    deploy: 871371fb9cb7c07e35bb91ba5053568b13f3a01f

---
+++ precache-manifest.2088d1b12c3e943eb75ac1f468617e7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e35aba17b8b77cc87a04ce5fa3c89ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9a0193a64008a338356",
+    "url": "./static/css/2.8ff24886.chunk.css"
+  },
+  {
+    "revision": "ddc53226df5a5b56c645",
+    "url": "./static/css/main.bbd72071.chunk.css"
+  },
+  {
+    "revision": "d9a0193a64008a338356",
+    "url": "./static/js/2.c4cf8523.chunk.js"
+  },
+  {
+    "revision": "1969650359612b8b27216359aee4b71f",
+    "url": "./static/js/2.c4cf8523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc53226df5a5b56c645",
+    "url": "./static/js/main.1aa458f8.chunk.js"
+  },
+  {
+    "revision": "f775eecf3e135a196909",
+    "url": "./static/js/runtime-main.81f53b81.js"
+  },
+  {
+    "revision": "255efbf6d06642bc6633048aadd5f638",
+    "url": "./static/media/CalvinUniv-vert-full-color-inverse.255efbf6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.63789a7ee02ed3dfb08c95689e7faf63.js"
+  "./precache-manifest.2088d1b12c3e943eb75ac1f468617e7b.js"
 );
 
 self.addEventListener('message', (event) => {
