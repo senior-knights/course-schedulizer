243d9cd28bef8e90f25594d38da69e2c2c7e705b
Syobnaf <bryantgeorge1@sbcglobal.net>
2021-04-01 09:40:31 -0400

    add department schedule prototype

---
+++ client-course-schedulizer/src/components/Tabs/DepartmentSchedule/DepartmentSchedule.tsx
@@ -0,0 +1,22 @@
+import { Schedule } from "components";
+import React, { useContext } from "react";
+import { getEvents } from "utilities";
+import { AppContext } from "utilities/contexts";
+import "./DepartmentSchedule.scss";
+
+/* Creates a list of Calendars to create the Department Schedule
+ */
+export const DepartmentSchedule = () => {
+  const {
+    appState: { departments, schedule },
+  } = useContext(AppContext);
+
+  return (
+    <>
+      <Schedule
+        calendarHeaders={departments.sort()}
+        groupedEvents={getEvents(schedule, "department")}
+      />
+    </>
+  );
+};
--- client-course-schedulizer/src/utilities/interfaces/appInterfaces.ts
+++ client-course-schedulizer/src/utilities/interfaces/appInterfaces.ts
@@ -19,6 +19,7 @@ export enum SchedulizerTab {
 export interface AppState {
   classes: string[];
   colorBy: ColorBy;
+  departments: string[];
   fileUrl: string;
   professors: string[];
   rooms: string[];
@@ -30,11 +31,12 @@ export interface AppState {
 }
 
 // Defaults for the app state when it launches, will try to load
-//  previous appState (with cleared fileUrl) to launch app from.
+//  previous appState to launch app from.
 //  If no previous state saved, will default to the object below.
 export const initialAppState: AppState = loadLocal("appState") || {
   classes: [],
   colorBy: 0,
+  departments: [],
   fileUrl: "",
   professors: [],
   rooms: [],
@@ -44,6 +46,20 @@ export const initialAppState: AppState = loadLocal("appState") || {
   slotMaxTime: "22:00",
   slotMinTime: "6:00",
 };
+
+// Ensure that these aren't undefined if they didn't exist in the previous app state
+if (!initialAppState.classes) {
+  initialAppState.departments = [];
+}
+if (!initialAppState.departments) {
+  initialAppState.departments = [];
+}
+if (!initialAppState.professors) {
+  initialAppState.departments = [];
+}
+if (!initialAppState.rooms) {
+  initialAppState.departments = [];
+}
 
 // structure of actions that can be sent to app dispatch
 export interface AppAction {
