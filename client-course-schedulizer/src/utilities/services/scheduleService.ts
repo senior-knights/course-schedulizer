8a5b1118605529bb9624975ddb2c521000bc5566
Syobnaf <bryantgeorge1@sbcglobal.net>
2021-04-08 09:43:47 -0400

    handle undefined department case

--- client-course-schedulizer/src/utilities/services/scheduleService.ts
+++ client-course-schedulizer/src/utilities/services/scheduleService.ts
@@ -40,7 +40,7 @@ export const getEvents = (
   const days: Day[] = enumArray(Day);
   const scheduleWithConflicts = findConflicts(schedule);
   forEach(scheduleWithConflicts.courses, (course) => {
-    const dept = course.department;
+    const dept = course.department ? course.department : "No Department";
     forEach(course.sections, (section) => {
       const sectionName = `${course.prefixes[0]}-${course.number}-${section.letter}`;
       forEach(section.instructors, (prof) => {
@@ -289,8 +289,9 @@ export const getInstructionalMethods = (schedule: Schedule) => {
 export const getDepts = (schedule: Schedule) => {
   const departments: string[] = [];
   forEach(schedule.courses, (course) => {
-    if (!departments.includes(course.department)) {
-      departments.push(course.department ? course.department : "");
+    const deptStr = course.department ? course.department : "No Department";
+    if (!departments.includes(deptStr)) {
+      departments.push(deptStr);
     }
   });
   return departments.sort();
