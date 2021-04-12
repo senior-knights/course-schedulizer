0948fc90d90bc5615e2a84a89d3561fa8566e082
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-04-12 12:47:35 -0400

    change <AppBar /> to <footer />. correct semantic tag

--- client-course-schedulizer/src/components/Footer/Footer.tsx
+++ client-course-schedulizer/src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Grid } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import moment from "moment";
 import React from "react";
 import "./Footer.scss";
@@ -10,10 +10,10 @@ import "./Footer.scss";
 */
 export const Footer = () => {
   return (
-    <AppBar className="app-footer" elevation={0} position="static">
+    <footer className="app-footer">
       <Grid container justify="flex-start">
         <Box p={2}>© {moment().year()} Senior Knights</Box>
       </Grid>
-    </AppBar>
+    </footer>
   );
 };
