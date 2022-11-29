dc775403cb09908a9c607e5dd39ec9b328fd5747
Randall Pruim <rpruim@gmail.com>
2022-11-29 11:48:40 -0500

    Updates

---
+++ precache-manifest.16a8b4da3e72e8919fa7cf31fd6ee551.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422db0d0d60edbe5c29c366df5695c29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "229d224eff9cb054e377",
+    "url": "./static/css/2.425337e9.chunk.css"
+  },
+  {
+    "revision": "3c862d937860f6a6721c",
+    "url": "./static/css/main.dfcbb2bf.chunk.css"
+  },
+  {
+    "revision": "229d224eff9cb054e377",
+    "url": "./static/js/2.f592ee00.chunk.js"
+  },
+  {
+    "revision": "1969650359612b8b27216359aee4b71f",
+    "url": "./static/js/2.f592ee00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c862d937860f6a6721c",
+    "url": "./static/js/main.517729d4.chunk.js"
+  },
+  {
+    "revision": "58aa3ef5055dc809f578",
+    "url": "./static/js/runtime-main.b080aff4.js"
+  },
+  {
+    "revision": "255efbf6d06642bc6633048aadd5f638",
+    "url": "./static/media/CalvinUniv-vert-full-color-inverse.255efbf6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.be36beead43ebd2b4fd365b466403f63.js"
+  "./precache-manifest.16a8b4da3e72e8919fa7cf31fd6ee551.js"
 );
 
 self.addEventListener('message', (event) => {
