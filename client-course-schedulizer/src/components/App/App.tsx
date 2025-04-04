7fde1b8abd97d48e14b8880c34368b7a1890d6c1
Yuese Li <nihao6633839@gmail.com>
2025-04-04 13:01:44 -0400

    ui fixes

--- client-course-schedulizer/src/components/App/App.tsx
+++ client-course-schedulizer/src/components/App/App.tsx
@@ -6,7 +6,31 @@ import { reducer } from "utilities";
 import { useLocal } from "utilities/hooks/useLocal";
 import { AppContext } from "utilities/contexts";
 import { initialAppState } from "utilities/interfaces";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import "./App.scss";
+
+/* Custom theme to ensure consistent UI elements */
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  props: {
+    MuiCheckbox: {
+      color: "primary",
+    },
+    MuiRadio: {
+      color: "primary",
+    },
+    MuiSwitch: {
+      color: "primary",
+    },
+  },
+});
 
 /* App with a HashRouter.
 Ref: https://medium.com/@bennirus/deploying-a-create-react-app-with-routing-to-github-pages-f386b6ce84c2
@@ -17,29 +41,31 @@ export const App = () => {
   const [appState, appDispatch] = useReducer(reducer(save), initialAppState);
 
   return (
-    <div className="App">
-      <HashRouter basename="/">
-        <Header />
-        <AppContext.Provider value={{ appDispatch, appState, isCSVLoading, setIsCSVLoading }}>
-          <div className="content-container">
-            <Switch>
-              <Route path="/about">
-                <AboutPage />
-              </Route>
-              <Route path="/help">
-                <HelpPage />
-              </Route>
-              {/* <Route path="/harmony">
-                <HarmonyPage />
-              </Route> */}
-              <Route path="/">
-                <SchedulizerPage />
-              </Route>
-            </Switch>
-          </div>
-        </AppContext.Provider>
-        <Footer />
-      </HashRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <HashRouter basename="/">
+          <Header />
+          <AppContext.Provider value={{ appDispatch, appState, isCSVLoading, setIsCSVLoading }}>
+            <div className="content-container">
+              <Switch>
+                <Route path="/about">
+                  <AboutPage />
+                </Route>
+                <Route path="/help">
+                  <HelpPage />
+                </Route>
+                {/* <Route path="/harmony">
+                  <HarmonyPage />
+                </Route> */}
+                <Route path="/">
+                  <SchedulizerPage />
+                </Route>
+              </Switch>
+            </div>
+          </AppContext.Provider>
+          <Footer />
+        </HashRouter>
+      </div>
+    </ThemeProvider>
   );
 };
