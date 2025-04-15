748626d8cf7ed7e6c0d61fc6acef41852fa1fcdd
kvlinden <kvlinden@calvin.edu>
2025-04-15 13:19:58 -0400

    removed formatting field list streamlined the help

--- client-course-schedulizer/src/components/pages/HelpPage/HelpPage.tsx
+++ client-course-schedulizer/src/components/pages/HelpPage/HelpPage.tsx
@@ -6,8 +6,6 @@ export const HelpPage = () => {
   return (
     <Page>
       <Functionality />
-      <FormatSegment />
-      <Faq />
     </Page>
   );
 };
@@ -44,9 +42,21 @@ const Functionality = () => {
           <strong>To edit an existing schedule:</strong>
           <ol>
             <li>
-              You can start with an existing schedule file (CSV or XLSX), or you can download a new
-              copy using the &ldquo;Schedulizer Course Sections&rdquo; report from{" "}
-              <code>reports.calvin.edu</code>.
+              Obtain a previous schedule in one of three ways:
+              <ul>
+                <li>
+                  Use a CSV file previously exported from an older version Course Schedulizer
+                  (before April 17, 2025).
+                </li>
+                <li>
+                  Use an Excel file exported from a newer version of Course Schedulizer (after April
+                  17, 2025).
+                </li>
+                <li>
+                  Export a schedule (in Excel) using the &ldquo;Schedulizer Course Sections&rdquo;
+                  report from <code>reports.calvin.edu</code>.
+                </li>
+              </ul>
             </li>
             <li>
               Load the schedule file by clicking either:
@@ -58,8 +68,11 @@ const Functionality = () => {
                 </li>
               </ul>
             </li>
+            <li>Use the standard schedule editing features to make the required changes.</li>
           </ol>
-          Use the standard schedule editing features to make the required changes.
+          In extreme cases, you can edit the data in the Excel file directly. However, this is not
+          recommended. The Schedulizer is designed to be used with the standard interface, and
+          editing the Excel file directly may result in errors or unexpected behavior.
           <br />
           <br />
           <strong>To save a schedule:</strong>
@@ -72,183 +85,18 @@ const Functionality = () => {
           one with the standard &ldquo;Schedule&rdquo; entries; one with &ldquo;Registrar
           Schedule&rdquo; formatted for the registrar; and one &ldquo;Metadata&rdquo; with
           additional schedule information.
-        </>
-      }
-      title="Help"
-    />
-  );
-};
-
-const FormatSegment = () => {
-  return (
-    <TextSection
-      body={
-        <>
-          In the web app, upload a CSV following the prescribed specifications (items marked with *
-          are optional and ignored, but were required in older versions of the app)
+          <br />
+          <br />
+          <strong>To get support for the Schedulizer:</strong>
           <ul>
             <li>
-              {" "}
-              <strong>Department</strong>: string (e.g., <code>Mathematics</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>Term</strong>: (FA | SP | IN) (e.g., <code>SP</code> for Spring)
-            </li>
-            <li>
-              {" "}
-              <strong>TermStart</strong>: mm/dd/yyyy (e.g., <code>3/29/2021</code> or{" "}
-              <code>12/1/2022</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>AcademicYear</strong>: yyyy (e.g., <code>2021</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>SectionName</strong>: SubjectCode-CourseNum-SectionCode (e.g.,{" "}
-              <code>MATH-252-B</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>SubjectCode</strong>: string (e.g., <code>MATH</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>CourseNum</strong>: string (e.g., <code>252</code> or <code>252L</code> for a
-              lab)
-            </li>
-            <li>
-              {" "}
-              <strong>SectionCode</strong>: string (e.g., <code>B</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>CourseLevelCode</strong>: pos num (e.g., <code>200</code> for a 200 level
-              course)
-            </li>
-            <li>
-              {" "}
-              <strong>MinimumCredits</strong>: pos num (e.g., <code>3</code> or <code>3.5</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>FacultyLoad</strong>: pos num (e.g., <code>4</code> or <code>4.5</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>Used</strong>: pos num (e.g., <code>20</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>Day10Used</strong>: pos num (e.g., <code>22</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>LocalMax</strong>: pos num (e.g., <code>25</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>GlobalMax</strong>: pos num (e.g., <code>30</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>RoomCapacity</strong>: pos num (e.g., <code>32</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>BuildingAndRoom</strong>: string (e.g., <code>HH 345</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>MeetingDays</strong>: M?T?W?(TH)?F? (e.g., <code>MWTHF</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>MeetingTime</strong>: xx:xx(AM | PM) - xx:xx(AM | PM) (e.g.,{" "}
-              <code>9:00AM - 9:50AM</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>SectionStartDate</strong>: mm/dd/yyyy (e.g., <code>3/29/2021</code> or{" "}
-              <code>12/1/2022</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>SectionEndDate</strong>: mm/dd/yyyy (e.g., <code>3/29/2021</code> or{" "}
-              <code>12/1/2022</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>Building</strong>: string (e.g., <code>HH</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>RoomNumber</strong>: string (e.g., <code>345</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>MeetingStart</strong>: xx:xx(AM | PM) (e.g., <code>2:30PM</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>MeetingStartInternal</strong>: xx:xx:xx 24-hour (e.g., <code>14:30:00</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>MeetingEnd</strong>: xx:xx(AM | PM) (e.g., <code>3:20PM</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>MeetingEndInternal</strong>: xx:xx:xx 24-hour (e.g., <code>13:20:00</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>Monday</strong>: <code>M</code> or empty
-            </li>
-            <li>
-              {" "}
-              *<strong>Tuesday</strong>: <code>T</code> or empty
-            </li>
-            <li>
-              {" "}
-              *<strong>Wednesday</strong>: <code>W</code> or empty
-            </li>
-            <li>
-              {" "}
-              *<strong>Thursday</strong>: <code>TH</code> or empty
-            </li>
-            <li>
-              {" "}
-              *<strong>Friday</strong>: <code>F</code> or empty
-            </li>
-            <li>
-              {" "}
-              <strong>ShortTitle</strong>: string (e.g., <code>Number Theory</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>Faculty</strong>: string (first and last) (e.g., <code>Paul Erdos</code>)
-            </li>
-            <li>
-              {" "}
-              *<strong>SectionStatus</strong>: string (e.g., <code>Active</code>)
-            </li>
-            <li>
-              {" "}
-              <strong>InstructionalMethod</strong>: <code>LEC</code>, <code>CPI</code>,{" "}
-              <code>IND</code>, <code>TUT</code>, or <code>SEM</code>
-            </li>
-            <li>
-              {" "}
-              <strong>DeliveryMode</strong>: <code>In-person</code>,{" "}
-              <code>Online (synchronous)</code>, <code>Online (asynchronous)</code>,{" "}
-              <code>Hybrid</code>
+              Contact the <a href="https://computing.calvin.edu/">Department of Computer Science</a>
+              .{" "}
             </li>
           </ul>
         </>
       }
-      title="Formatting"
+      title="Help"
     />
   );
 };
