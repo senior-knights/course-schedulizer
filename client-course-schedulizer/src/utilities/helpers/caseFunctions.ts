c4463a8a624d1fdde07cf17014dbd5f1e95a3242
Yuese Li <nihao6633839@gmail.com>
2025-04-22 15:46:07 -0400

    fixed conflicts

--- client-course-schedulizer/src/utilities/helpers/caseFunctions.ts
+++ client-course-schedulizer/src/utilities/helpers/caseFunctions.ts
@@ -1,6 +1,6 @@
 import { cloneDeep, forEach } from "lodash";
 import moment from "moment";
-// Case is no longer exported in runtypes 7.0.4
+//import { Case } from "runtypes";
 import { emptyMeeting } from "utilities/constants";
 import { Course, Day, Meeting, Section, SemesterLength, Term } from "utilities/interfaces";
 
