18b36fd58e29810bb90b0b60ddfd80e20be8cd21
Kai Doo <endmsgur@gmail.com>
2026-02-09 12:44:44 -0500

    Deleting Export Button by re-git cloning the whole repo

--- client-course-schedulizer/src/components/Tabs/tabComponents/CSVActions/CSVActions.tsx
+++ client-course-schedulizer/src/components/Tabs/tabComponents/CSVActions/CSVActions.tsx
@@ -39,9 +39,6 @@ export const CSVActions = () => {
         <ImportInputWrapper isAdditiveImport>
           <MenuItem button>IMPORT CONSTRAINTS</MenuItem>
         </ImportInputWrapper>
-        <MenuItem button onClick={onFullExportClick}>
-          EXPORT CSV
-        </MenuItem>
         <MenuItem button onClick={onExportExcelClick}>
           EXPORT EXCEL
         </MenuItem>
--- client-course-schedulizer/src/components/Tabs/tabComponents/CSVActions/index.ts
+++ client-course-schedulizer/src/components/Tabs/tabComponents/CSVActions/index.ts
@@ -1,4 +1,2 @@
 export * from "./CSVActions";
-export * from "./ExportButton";
-export * from "./FullExportButton";
 export * from "./ImportButton";
